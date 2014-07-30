8875601c17d36aa375299290bf673c039bfa00da
YanJun Yang <yangyj.ee@gmail.com>
2014-07-30 15:38:54 +0800

    Key shows real key name

--- key/key.c
+++ key/key.c
@@ -7,7 +7,8 @@ __xdata __at (0x8001) unsigned char cs;
 __code const unsigned char segs[] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,
     0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x86, 0x8E};
 __code const unsigned char css[] = {0x7, 0xB, 0xD, 0xE};
-__code const unsigned char dec[] = {0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0}; 
+__code const unsigned char dec[] = {0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0};
+__code const unsigned char trans[] = {0xC, 9, 5, 1, 0xD, 0, 6, 2, 0xE, 0xA, 7, 3, 0xF, 0xB, 8, 4};
 
 void main(void)
 {
@@ -32,7 +33,7 @@ void main(void)
         i = ~P4;
         i >>= 4;
         key = key + dec[i];
-        seg = segs[key & 0xF]; // show on display
+        seg = segs[trans[key & 0xF]]; // show on display
         Delay(1000);
     }
 }
