fb4ee3dc54dbaa22c41f4f041e1360d40eab8036
YanJun Yang <yangyj.ee@gmail.com>
2014-07-30 15:11:38 +0800

    New board use P7.7 to drive LED

--- led/led.c
+++ led/led.c
@@ -7,7 +7,7 @@ void main(void)
     WDTCN = 0xad; // disable watchdog
     P74OUT = 0x80; // push-pull
     while (1) {
-        P7 = 0x40;
+        P7 = 0xC0;
         Delay(20000);
         P7 = 0;
         Delay(20000);
